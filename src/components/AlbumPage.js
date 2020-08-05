2202f7e07dc0604168e8e4e0ab40c1f16cc7f852
Keenan McDonald <keemcd@gmail.com>
2020-08-05 11:56:32 -0500

    work on album page

--- src/components/AlbumPage.js
+++ src/components/AlbumPage.js
@@ -11,12 +11,15 @@ function AlbumPage(props){
                 <div className='col-sm-12 col-md-6'> 
                     <img className='album-page-cover' alt={`${props.title} cover`} src={require(`../images/covers/${props.cover}`)}/>
                 </div>
-                <div className='col-sm-12 col-md-6'>
+                <div className='title-purchase-container col-sm-12 col-md-6'>
                     <h1 className='album-page-artist'>{props.artist.toUpperCase()}</h1>
                     <h2 className='album-page-title'>{props.title}</h2>
                     <p className='album-page-code'>{props.code}</p>
                     {ReactHtmlParser(props.purchase[0].link)}
                 </div> 
+            </div>
+            <div className='album-page-description-container'>
+                <p className='album-page-description'>{props.description}</p> 
             </div>
             <div className='row'></div>
         </section>
