6239e6d33920d87712c7f1703c1e2a08a9259907
Keenan McDonald <keemcd@gmail.com>
2020-08-07 17:52:58 -0500

    added spotify and tracklist

--- src/components/AlbumPage.js
+++ src/components/AlbumPage.js
@@ -3,64 +3,107 @@ import '../css/AlbumPage.css'
 import ReactHtmlParser from 'react-html-parser'
 
 
-function AlbumPage(props){
+class AlbumPage extends React.Component{
 
-    const purchase = []
-
-    for (let i = 0; i < props.purchase.length; i++){
-        purchase.push(
-            <div key={i} className='purchase-form-container'>
-                <form id={`paypal${i}`} target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">           
-                    <input type="hidden" name="cmd" value="_s-xclick"/>
-                    <input type="hidden" name="hosted_button_id" value={props.purchase[i].button_id}/>
-                    <div className='purchase-button-container'>
-                        <button type='submit' form={`paypal${i}`} className='album-page-purchase-button format'>{props.purchase[i].format.toUpperCase()}</button>
-                        <button type='submit' form={`paypal${i}`} className='album-page-purchase-button price'>{props.purchase[i].price}</button>
-                    </div>
-                </form>
-            </div> 
-        )
+    constructor(props){
+        super(props)
+        this.description = React.createRef()
+        this.state = {
+            width: 300,
+            height: 400,
+        }
     }
 
-    const description = []
-
-    for (let i = 0; i < props.description.length; i++){
-        description.push(<p key={i} className='album-page-description'>{props.description[i]}</p> )
+    componentDidMount(){
+        this.setState({
+            width: this.description.current.offsetWidth * .8,
+            height: this.description.current.offsetHeight
+        })
     }
 
-    return (
-        <section className='album-page'>
-            <div className='album-page-main row'>
-                <div className='col-sm-12 col-md-6'> 
-                    <img className='album-page-cover' alt={`${props.title} cover`} src={require(`../images/covers/${props.cover}`)}/>
-                </div>
-                <div className='title-purchase-container col-sm-12 col-md-6'>
-                    <h1 className='album-page-artist'>{props.artist.toUpperCase()}</h1>
-                    <h2 className='album-page-title'>{props.title}</h2>
-                    <p className='album-page-code'>{props.code}</p>
-                    <div className='album-page-purchase-container'>
-                        <p className='album-page-purchase'>Purchase</p>
-                        {purchase}
-                    </div> 
-                    {props.quote ?
-                    <div className='quote-container'>
-                        <p className='quote'>{props.quote}</p>
-                        <p className='quote-attribution'>{props.quote_attribution}</p>
-                    </div>
-                    : ''}
+    render(){
+        const purchase = []
+        const description = []
+        const tracklist = []
+
+        for (let i = 0; i < this.props.purchase.length; i++){
+            purchase.push(
+                <div key={i} className='purchase-form-container'>
+                    <form id={`paypal${i}`} target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">           
+                        <input type="hidden" name="cmd" value="_s-xclick"/>
+                        <input type="hidden" name="hosted_button_id" value={this.props.purchase[i].button_id}/>
+                        <div className='purchase-button-container'>
+                            <button type='submit' form={`paypal${i}`} className='album-page-purchase-button format'>{this.props.purchase[i].format.toUpperCase()}</button>
+                            <button type='submit' form={`paypal${i}`} className='album-page-purchase-button price'>{this.props.purchase[i].price}</button>
+                        </div>
+                    </form>
                 </div> 
-            </div>
-            <div className='album-page-description-container'>
-                {description} 
-                <p className='notes'>{props.notes}</p>
-            </div>
-            {props.video ? 
-                <div className='album-page-video-container'>
-                    {ReactHtmlParser(props.video)}
+            )
+        }
+    
+    
+        for (let i = 0; i < this.props.description.length; i++){
+            description.push(<p key={i} className='album-page-description'>{this.props.description[i]}</p> )
+        }
+
+        if (this.props.tracklist){
+            for (let i = 0; i < this.props.tracklist.length; i++){
+                tracklist.push(<li key={i} className='album-page-track'>{this.props.tracklist[i]}</li>)
+            }
+        }
+    
+        return (
+            <section className='album-page'>
+                <div className='album-page-main row'>
+                    <div className='col-sm-12 col-md-6'> 
+                        <img className='album-page-cover' alt={`${this.props.title} cover`} src={require(`../images/covers/${this.props.cover}`)}/>
+                    </div>
+                    <div className='title-purchase-container col-sm-12 col-md-6'>
+                        <h1 className='album-page-artist'>{this.props.artist.toUpperCase()}</h1>
+                        <h2 className='album-page-title'>{this.props.title}</h2>
+                        <p className='album-page-code'>{this.props.code}</p>
+                        <div className='album-page-purchase-container'>
+                            <p className='album-page-purchase'>Purchase</p>
+                            {purchase}
+                        </div> 
+                        {this.props.quote ?
+                        <div className='quote-container'>
+                            <p className='quote'>{this.props.quote}</p>
+                            <p className='quote-attribution'>{this.props.quote_attribution}</p>
+                        </div>
+                        : ''}
+                    </div> 
                 </div>
-            : ''}
-        </section>
-    )
+                <div className='album-page-description-tracklist-container row'>
+                    <div ref={this.description} className={`album-page-description-container ${this.props.spotify || this.props.tracklist ? 'col-sm-6' : ''}`}>
+                        {description} 
+                    </div>
+                    {this.props.spotify ? 
+                        <div className='album-page-spotify col-sm-6'>
+                            <iframe title='spotify' src={`https://open.spotify.com/embed/album/${this.props.spotify}`} width={this.state.width} height={this.state.height} frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                        </div>
+                    : (this.props.tracklist ?
+                        <div className='album-page-tracklist-container col-sm-6'>
+                            <div className='album-page-tracklist-title-container'>
+                                <h5 className='album-page-tracklist-title'>tracklist</h5>
+                            </div>
+                            <ol className='album-page-tracklist'>
+                                {tracklist}
+                            </ol>
+                        </div>
+                    : '')}
+                </div>
+                <p className='notes'>{this.props.notes}</p>
+                {this.props.video ? 
+                    <div className='album-page-video-container'>
+                        {ReactHtmlParser(this.props.video)}
+                    </div>
+                : ''}
+            </section>
+        )
+    
+    }
+
 }
 
 export default AlbumPage
