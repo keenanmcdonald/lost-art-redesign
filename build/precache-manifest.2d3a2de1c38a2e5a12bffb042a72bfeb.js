c2f9b21f69b23bafabfadbf53462bda887cf602c
Keenan McDonald <keemcd@gmail.com>
2021-08-07 10:38:09 -0500

    build removed from gitignore

---
+++ build/precache-manifest.2d3a2de1c38a2e5a12bffb042a72bfeb.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8d82b08a41d5407c73c28d2045a6acf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9417907b527bd0a8cf25",
+    "url": "/static/css/main.c4f72149.chunk.css"
+  },
+  {
+    "revision": "b81d12eff6dc6c703582",
+    "url": "/static/js/2.2044bacb.chunk.js"
+  },
+  {
+    "revision": "d42c58549d938108abf780d97cd26873",
+    "url": "/static/js/2.2044bacb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9417907b527bd0a8cf25",
+    "url": "/static/js/main.976a570f.chunk.js"
+  },
+  {
+    "revision": "0d667b7a7971d70a81c3",
+    "url": "/static/js/runtime-main.aad2254c.js"
+  },
+  {
+    "revision": "283fcf8474be8f3baf4b348540e55e83",
+    "url": "/static/media/Oakwood-Clean.283fcf84.otf"
+  },
+  {
+    "revision": "19465d5cb88a2503a1fe5cb7d4d2e1ea",
+    "url": "/static/media/Oakwood-Rough.19465d5c.otf"
+  },
+  {
+    "revision": "a85f0656fc21d06ed7175af1d606ba60",
+    "url": "/static/media/Oakwood-RoughShadow.a85f0656.otf"
+  },
+  {
+    "revision": "24df9e943920c298e9a7d8a9ccdd0b9f",
+    "url": "/static/media/OldGrowth-Regular.24df9e94.otf"
+  },
+  {
+    "revision": "0791c66044b209ff9441d649e62111d1",
+    "url": "/static/media/austin-outhouse-cover.0791c660.jpg"
+  },
+  {
+    "revision": "749d69d12332c24b14815340dcc4cfc2",
+    "url": "/static/media/bill-neely-cover.749d69d1.jpg"
+  },
+  {
+    "revision": "9962b42e4aa500bd1e01ae423a296ff4",
+    "url": "/static/media/clay-pigeons-cover.9962b42e.jpg"
+  },
+  {
+    "revision": "fb49cd13a53e50536a46ac95efb92962",
+    "url": "/static/media/cold-cold-world-cover.fb49cd13.png"
+  },
+  {
+    "revision": "cfc53d00f61f9379044d0f0c2c66f110",
+    "url": "/static/media/duct-tape-deluxe-cover.cfc53d00.jpg"
+  },
+  {
+    "revision": "e2077db1156a5f6a6c7ad7d28212a697",
+    "url": "/static/media/duct-tape-messiah-cover.e2077db1.jpg"
+  },
+  {
+    "revision": "3e04c7c0ebc27e785ff203e545fd17d1",
+    "url": "/static/media/foley-slim.3e04c7c0.jpg"
+  },
+  {
+    "revision": "d13b289fd759e545851fdc0ce0e9beb8",
+    "url": "/static/media/foley.d13b289f.jpg"
+  },
+  {
+    "revision": "207af3c1d03f1053e380213f3be83453",
+    "url": "/static/media/logo-horizontal.207af3c1.png"
+  },
+  {
+    "revision": "a4a66674c6a394b5ca79cfd59a9f8fa2",
+    "url": "/static/media/logo-mic.a4a66674.png"
+  },
+  {
+    "revision": "3c2423751a4f1347dd44fadb113c7084",
+    "url": "/static/media/muscle-shoals-cover.3c242375.jpg"
+  },
+  {
+    "revision": "d04f8733c56b63aa8b42513c5456b4fb",
+    "url": "/static/media/outhouse-sticker.d04f8733.png"
+  },
+  {
+    "revision": "a91fc04c1084b1e4d534a5dad9407ead",
+    "url": "/static/media/oval-room-cover.a91fc04c.jpg"
+  },
+  {
+    "revision": "2b6741e4272b5ba38084b25c11e4e13a",
+    "url": "/static/media/sittin-by-the-road-cover.2b6741e4.jpg"
+  }
+]);
