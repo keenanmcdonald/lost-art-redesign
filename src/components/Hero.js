5b0a86e12c2097b756706dd733cd03a7150506c6
Keenan McDonald <keemcd@gmail.com>
2022-06-19 17:45:46 -0500

    restore blaze hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,16 +3,17 @@ import {withRouter} from 'react-router-dom'
 
 function Hero(props){
     return (
-        <section id='hero' className='row no-gutters' onClick={() => props.history.push('/igottheblues')}>
-            <div className='col-8'>
-                <div className='hero-text'>
-                    <h1 className='hero-artist'>CODY HUBACH</h1> 
-                    <h2 className='hero-title'>I GOT THE BLUES</h2>
-                    <h3 className='hero-subtitle'>available on cd and streaming platforms now</h3>
-                </div>
+        <section id='hero' className='row no-gutters' onClick={() => props.history.push('/austinouthouse')}>
+            <div className='col-5 photo-container'>
+                <img className='blaze-photo' alt='blaze in profile' src={require('../images/foley.jpg')}/>
+                <img className='blaze-photo-slim' alt='blaze in profile' src={require('../images/foley-slim.jpg')}/>
             </div>
-            <div className='col-4 photo-container'>
-                <img className='blaze-photo' alt='cody hubach looking forward' src={require('../images/cody-cutout.png')}/>
+            <div className='col-6'>
+                <div className='hero-text'>
+                    <h1 className='hero-artist'>blaze foley</h1> 
+                    <h2 className='hero-title'>live at the austin outhouse</h2>
+                    <h3 className='hero-subtitle'>available on vinyl for the first time</h3>
+                </div>
             </div>
         </section>
     )
