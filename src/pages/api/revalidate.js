995924c50473dfe1fbb92fc34a0038b01274ac66
Keenan McDonald <keemcd@gmail.com>
2023-08-25 09:33:39 -0500

    log path

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const path = req.query.path
-  console.log(path)
+  console.info({ path })
   await res.revalidate(path)
   return res.json({ revalidate: true })
 }
